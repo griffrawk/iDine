bd4cfa9303f01a1a4fd114072459fa96606b2045
Andy Griffiths <griffiths_andy@icloud.com>
2023-01-31 01:03:16 +0000

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Andy Griffiths on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
