d5263c4f36fdc699dab140d15e294f6f74a527bd
Andy Griffiths <griffiths_andy@icloud.com>
2023-02-02 01:10:51 +0000

    State & Environment Objects

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,37 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Andy Griffiths on 01/02/2023.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    @State private var paymentType = "Cash"
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                        
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView()
+            .environmentObject(Order())
+    }
+}
---
+++ iDine/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Andy Griffiths on 31/01/2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
