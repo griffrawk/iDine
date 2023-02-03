3c7d5eb9bce50ab03dc433514d6f6b81bda28258
Andy Griffiths <griffiths_andy@icloud.com>
2023-02-03 01:36:50 +0000

    Loyalty card toggle

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -13,14 +13,20 @@ struct CheckoutView: View {
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash"
     
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    
     var body: some View {
-        VStack {
+        Form      {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
-                        
                     }
+                }
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
                 }
             }
         }
