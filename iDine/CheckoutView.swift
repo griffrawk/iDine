d8d74c44082864504b5914f020fdd9955f6d6ec1
Andy Griffiths <griffiths_andy@icloud.com>
2023-02-03 23:59:08 +0000

    Complete as per end of video series

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -16,6 +16,17 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     
+    let tipAmounts = [10,15,20,25,0]
+    @State private var tipAmount = 15
+    
+    @State private var showingPaymentAlert = false
+    
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     var body: some View {
         Form      {
             Section {
@@ -29,9 +40,27 @@ struct CheckoutView: View {
                     TextField("Enter your iDine ID", text: $loyaltyNumber)
                 }
             }
+            Section("Add a tip?") {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            Section("Total: \(totalPrice)") {
+                Button("Confirm Order"){
+                    showingPaymentAlert.toggle()
+                }
+            }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order confirmed", isPresented: $showingPaymentAlert) {
+            // add buttons
+        } message: {
+            Text("Your total price was \(totalPrice) - Thank you!")
+        }
     }
 }
 
